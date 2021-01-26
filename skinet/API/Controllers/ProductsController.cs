8f84599b13e81af099865e0eec79087dcea953eb
kelldezim <kelldezim@gmail.com>
2021-01-26 23:05:49 +0100

    Setup_EntityFramework_and_creating_DBMigration_and_Context

---
+++ skinet/API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts(){
+
+            var products = _context.Products.ToList(); // performing select query on our db
+
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public string GetProduct(int id){
+            return "single product";
+        }
+    }
+}
