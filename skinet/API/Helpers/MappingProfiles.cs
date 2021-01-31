38e64f03184f304d7020c3d92e7c344cb7140466
kelldezim <kelldezim@gmail.com>
2021-01-31 11:20:35 +0100

    Implementing_DTO_and_AutoMapper_Plus_Adding_Random_Pics_for_dev_purposes

---
+++ skinet/API/Helpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+using API.Controllers.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        { //if there is object type data to mapp it requires detailed mapping via flagging record by ForMember()
+            CreateMap<Product, ProductToReturnDto>()
+                .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
+        }
+    }
+}
