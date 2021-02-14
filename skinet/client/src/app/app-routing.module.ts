aa1af195e6ff334c36de9fc8226cd01cac2380b4
Grzegorz Kapalka <BWRG73@motorolasolutions.com>
2021-02-15 00:42:07 +0100

    Added-ErrorTestComp-For-ClientSiteTesting

--- skinet/client/src/app/app-routing.module.ts
+++ skinet/client/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'shop', loadChildren: ()=> import('./shop/shop.module').then(mod =>  mod.ShopModule)},
+  {path: 'test-error', component: TestErrorComponent},
+  {path: 'shop', loadChildren: ()=> import('./shop/shop.module').then(mod =>  mod.ShopModule)}, // Lazy-Loading
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
