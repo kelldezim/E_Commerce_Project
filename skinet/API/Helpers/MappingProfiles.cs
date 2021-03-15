d2e5e895e535439fc15efaa06bd1daa6ad07ca3a
Grzegorz Kapalka <BWRG73@motorolasolutions.com>
2021-03-15 14:45:21 +0100

    authotization_and_account_controllerrequest

--- skinet/API/Helpers/MappingProfiles.cs
+++ skinet/API/Helpers/MappingProfiles.cs
@@ -1,6 +1,7 @@
 using API.Controllers.Dtos;
 using AutoMapper;
 using Core.Entities;
+using Core.Entities.Identity;
 
 namespace API.Helpers
 {
@@ -12,6 +13,9 @@ namespace API.Helpers
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
                 .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
                 .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
+
+            // same props so automapper handles it
+            CreateMap<Address, AddressDto>().ReverseMap(); 
         }
     }
 }
